6109d5d79470019e1c06c7ce4e7adeb89ecba69a
GiFRforever <93822902+GiFRforever@users.noreply.github.com>
2022-12-03 21:16:14 +0100

    zacatek

--- sendmail.py
+++ sendmail.py
@@ -9,29 +9,29 @@ from getpass import getpass
 
 
 class SendMail:
-    def __init__(self):
+    def __init__(self) -> None:
         try:  # load credentials from file
             with open("credentials.pickle", "rb") as f:
-                self.password = pickle.load(f)
+                self.password: str = pickle.load(f)
         except FileNotFoundError:
             self.password = getpass("Password: ")
             if input("Save password? [y/n]: ").lower() == "y":
                 with open("credentials.pickle", "wb") as f:
                     pickle.dump(self.password, f)
-        self.sender_email = "odpadzreklam@seznam.cz"
-        self.receiver_email = "frantisek.clupny@email.cz"
-        self.smtp_server = "smtp.seznam.cz"
-        self.port = 465  # For starttls
+        self.sender_email: str = "odpadzreklam@seznam.cz"
+        self.receiver_email: str = "frantisek.clupny@email.cz"
+        self.smtp_server: str = "smtp.seznam.cz"
+        self.port: int = 465  # For starttls
 
-    def send_mail(self, file: str):
+    def send_mail(self, file: str) -> None:
 
-        filename = file.split("/")[-1]  # exract filename from path
+        filename: str = file.split("/")[-1]  # exract filename from path
 
-        self.subject = f"Teploty z {filename}"
-        self.body = "Data jsou v příloze"
+        self.subject: str = f"Teploty z {filename}"
+        self.body: str = "Data jsou v příloze"
 
         # Create a multipart self.message and set headers
-        self.message = MIMEMultipart()
+        self.message: MIMEMultipart = MIMEMultipart()
         self.message["From"] = self.sender_email
         self.message["To"] = self.receiver_email
         self.message["Subject"] = self.subject
@@ -44,7 +44,7 @@ class SendMail:
         with open(file, "rb") as attachment:
             # Add file as application/octet-stream
             # Email client can usually download this automatically as attachment
-            part = MIMEBase("application", "octet-stream")
+            part: MIMEBase = MIMEBase("application", "octet-stream")
             part.set_payload(attachment.read())
 
         # Encode file in ASCII characters to send by email
@@ -55,7 +55,7 @@ class SendMail:
 
         # Add attachment to self.message and convert self.message to string
         self.message.attach(part)
-        text = self.message.as_string()
+        text: str = self.message.as_string()
 
         # Log in to server using secure context and send email
         context = ssl.create_default_context()
