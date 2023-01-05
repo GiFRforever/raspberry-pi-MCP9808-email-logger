da21b3acbe4d4dcd953e49ab3236b1c03bdc07e2
GiFRforever <93822902+GiFRforever@users.noreply.github.com>
2023-01-05 07:18:18 +0100

    Create makeconfig.sh

---
+++ makeconfig.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Read the template file
+TEMPLATE=$(<"configtemplate.json")
+
+# Substitute the variables in the template with the provided values
+OUTPUT="$TEMPLATE"
+for VAR in $(grep -o "\${[^}]*}" "configtemplate.json" | sed 's/[${}]//g'); do
+  read -p "Enter value for $VAR: " VALUE
+  OUTPUT="${OUTPUT//\$\{$VAR\}/$VALUE}"
+done
+
+# Write the output to the specified file
+echo "$OUTPUT" > "config.json"
