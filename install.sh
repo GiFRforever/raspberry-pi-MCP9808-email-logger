1512a27aa13edbff7b0878db0f1d6d9415510f7a
GiFRforever <gifrforever@gmail.com>
2023-01-12 20:19:31 +0100

    maybe

--- install.sh
+++ install.sh
@@ -1,59 +1,46 @@
 #!/bin/bash
-
 # Exit immediately if a command returns a non-zero exit code
 set -e
-
 # Check if the script is being run as root
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
-
 # Start the makeconfig, unless service is running
 if ! systemctl is-active --quiet templogger.service; then
   # Start the other makeconfig.sh in the foreground
-  bash makeconfig.sh
-  
+  makeconfig.sh
   # Store the PID of the makeconfig.sh
   OTHER_SCRIPT_PID=$! 
 fi
-
 # Check if the required packages are already installed
 REQUIRED_PACKAGES=`grep -E "^[^#]" requirements.txt`
 INSTALLED_PACKAGES=`pip freeze`
-
 # Install any missing packages
 for package in $REQUIRED_PACKAGES; do
   if ! echo "$INSTALLED_PACKAGES" | grep -q "$package"; then
     pip install "$package"
   fi
 done
-
 # Copy the service file and enable it
 cp templogger.service /lib/systemd/system/
 chmod 644 /lib/systemd/system/templogger.service
-
 # Replace the working directory placeholder in the service file with the current working directory
 sed -i "s#{CWD}#$(pwd)#" /lib/systemd/system/templogger.service
-
 systemctl daemon-reload
 systemctl enable templogger.service
-
 # Start the service, unless it's already running
 if ! systemctl is-active --quiet templogger.service; then
   # Wait for the other script to finish
   wait $OTHER_SCRIPT_PID
   systemctl start templogger.service
 fi
-
 # Set up the aliases, unless they already exist
 if ! grep -q "alias sm=" ~/.bashrc; then
   echo "alias sm='python /home/pi/templogger/sendmail.py'" >> ~/.bashrc
 fi
-
 if ! grep -q "alias rt=" ~/.bashrc; then
   echo "alias rt='python /home/pi/templogger/readtemp.py'" >> ~/.bashrc
 fi
-
 # Reload the bashrc file to apply the changes
 source ~/.bashrc
